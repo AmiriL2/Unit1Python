28910d4698fe12404c074cace88df4a45746553c
amiril2 <alayne@brooklynsteamcenter.org>
2023-09-19 09:50:35 -0400

    bench

---
+++ benchmark.py
@@ -0,0 +1,47 @@
+# Exercise 1
+print ("Amiri")
+
+# Exercise 2
+favoriteShow = "Regular Show"
+
+# Exercise 3
+
+Height = 6
+Age = 17
+
+print (Height * Age)
+
+# Exercise 4
+
+print("im going to break")
+
+#Exercise 5
+
+fav_food = ("pizza", "chicken","french fries")
+
+# Exersice 6
+
+if Age >= 21: 
+    print ("you can rent car")
+else:
+     print("your beat")
+
+age = 19
+
+# Exercise 7 
+input("please enter your name")
+
+name = input()
+
+print("hello" + name)
+
+# Exercise 8 
+for food in fav_food:
+
+# Exercise 9 
+x = 4
+y = 5
+while y < x:
+    print("lol")
+
+
